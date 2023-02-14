1936a6eeb181a4ee2ea5907b2c7b4988245d15c6
Takako Onozuka <onozukatakako@gmail.com>
2023-02-14 21:39:05 +0000

    added money input

--- src/components/Countries/USA.js
+++ src/components/Countries/USA.js
@@ -1,17 +1,42 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./CountryResult.css";
 import Button from "../reusable/Button";
+import Modal from "../Modal/Modal";
+import ReactDOM from 'react-dom/client';
 
 const USA = ({ifOnlyStocks}) => {
 
+    const [show, setShow] = useState(false)
 
+    function MoneyInput() {
+        return (
+          <form>
+            <label>Choose Amount:
+              <input type="number" />
+            </label>
+            <label>Choose Currency:
+              <select>
+                <option>USD</option>
+                <option>GBP</option>
+                <option>JPY</option>
+              </select>
+            </label>
+          </form>
+        )
+      }
+      
+      
 
     const USAlist = ifOnlyStocks.map((stock, id) => {
         if(stock.country === "USA") {
             return (
             <ul key ={id} className = "result-list">    
-            <li className="result-item">{stock.companyName}<Button text="If Only..."></Button></li>
+            <li className="result-item">{stock.companyName}
+            {/* <button>ShowModal</button> <Modal /> */}
+            <Button text="If Only..." value={stock.companyName} onClick={()=>setShow(true)}>
+            </Button><Modal onClose={()=>setShow(false)} show={show}/>
+            </li>
             </ul>
             )    
         }
@@ -20,6 +45,7 @@ const USA = ({ifOnlyStocks}) => {
     return (
         <>
         <h3>USA Stock List</h3>
+       <MoneyInput/>
         {USAlist}
         </>
 
---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,32 @@
+import React, {useEffect} from "react";
+import "./Modal.css";
+
+
+const Modal = props => {
+
+    if (!props.show) {
+        return null
+    }
+
+    return ( 
+        <div className="modal" onClick={props.onClose}>
+            <div className="modal-content" onClick={e => e.stopPropagation()}>
+                <div className="modal-header">
+                    <div className="modal-title">
+                        <p>
+                            If you had only invested in ...
+                        </p>
+                    </div>
+                    <div className="modal-body">
+                        Modal content
+                    </div>
+                    <div className="modal-footer">
+                        <button onClick ={props.onClose} className="button">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default Modal;
